f8caaadea105f3de226b2e3b4a0bdf85ae3e2bc6
NIRMALKUMAR V <nirmalkumarcc@gmail.com>
2025-11-10 18:45:18 +0530

    Create script.js

---
+++ color_picker/script.js
@@ -0,0 +1,22 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let clr = document.getElementById("selectedColorHexCode ");
+
+function button1() {
+    colorPickerContainer.style.backgroundColor = "#e0e0e0";
+    clr.textContent = "#e0e0e0";
+}
+
+function button2() {
+    colorPickerContainer.style.backgroundColor = "#6fcf97";
+    clr.textContent = "#6fcf97";
+}
+
+function button3() {
+    colorPickerContainer.style.backgroundColor = "#56ccf2";
+    clr.textContent = "#56ccf2";
+}
+
+function button4() {
+    colorPickerContainer.style.backgroundColor = "#bb6bd9";
+    clr.textContent = "#bb6bd9";
+}
